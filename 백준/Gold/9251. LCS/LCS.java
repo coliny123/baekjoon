c374cd3b4305e42f5cf13b9fd423af5b8a409618
김도환 <87745117+coliny123@users.noreply.github.com>
2025-01-15 10:20:15 +0900

    [Gold V] Title: LCS, Time: 140 ms, Memory: 18536 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -1,21 +1,20 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         // 코드를 작성해주세요
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        String a = sc.nextLine();
-        String b = sc.nextLine();
+        String first = br.readLine();
+        String second = br.readLine();
         
-        int aLength = a.length();
-        int bLength = b.length();
         
-        int[][] dp = new int[aLength+1][bLength+1];
+        int[][] dp = new int[first.length()+1][second.length()+1];
         
-        for(int i=1; i<=aLength; i++){
-            for(int j=1; j<=bLength; j++){
-                if(a.charAt(i-1) == b.charAt(j-1)){
+        for(int i=1; i<=first.length(); i++){
+            for(int j=1; j<=second.length(); j++){
+                if(first.charAt(i-1) == second.charAt(j-1)){
                     dp[i][j] = dp[i-1][j-1] + 1;
                 }else{
                     dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
@@ -23,9 +22,14 @@ public class Main {
             }
         }
         
+        // for(int i=0; i<=first.length(); i++){
+            // for(int j=0; j<=second.length(); j++){
+                // System.out.print(dp[i][j] + " ");
+            // }
+            // System.out.println();
+        // }
         
-        System.out.println(dp[aLength][bLength]);
-        
+        System.out.println(dp[first.length()][second.length()]);
         
     }
 }
