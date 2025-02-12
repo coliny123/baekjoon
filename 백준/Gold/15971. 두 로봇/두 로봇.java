8603f07e68550c495294cc44b94a342fc67cc460
김도환 <87745117+coliny123@users.noreply.github.com>
2025-02-12 10:43:16 +0900

    [Gold IV] Title: 두 로봇, Time: 536 ms, Memory: 76164 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/15971. 두 로봇/두 로봇.java
+++ 백준/Gold/15971. 두 로봇/두 로봇.java
@@ -34,26 +34,25 @@ public class Main {
     
     public static int bfs(int st, int ed){
         Queue<int[]> q = new LinkedList<>();
-        int[] distance = new int[N];
-        q.add(new int[]{st, 0, 0});
-        Arrays.fill(distance, 100000);
-        distance[st] = 0;
+        boolean[] visited = new boolean[N];
+        q.add(new int[]{st, 0, 0});   // 인덱스, 합, 최댓값
+        visited[st] = true;
         
         int answer = -1;
         while(!q.isEmpty()){
             int[] cur = q.poll();
-            // System.out.println(cur[0] + " " + cur[1] + " " + cur[2]);
             if(cur[0] == ed){
                 answer = cur[1] - cur[2];
-                // System.out.println(cur[1] + " " + cur[2]);
                 break;
             }
             
             for(int i=0; i<graph[cur[0]].size(); i++){
-                int[] nx = graph[cur[0]].get(i);
-                if(distance[nx[0]] > distance[cur[0]] + nx[1]){
-                    distance[nx[0]] = distance[cur[0]] + nx[1];
-                    q.add(new int[]{nx[0], distance[nx[0]], Math.max(cur[2], nx[1])});
+                int[] next = graph[cur[0]].get(i);
+                int nx = next[0];
+                int dist = next[1];
+                if(!visited[nx]){
+                    visited[nx] = true;
+                    q.add(new int[]{nx, cur[1] + dist, Math.max(cur[2], dist)});
                 }
             }
         }
