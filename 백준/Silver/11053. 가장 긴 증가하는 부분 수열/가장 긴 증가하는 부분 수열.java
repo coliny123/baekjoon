bbf3621c370b283b023fab91435a3710951d6944
김도환 <87745117+coliny123@users.noreply.github.com>
2025-02-13 10:14:18 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 108 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,28 +1,57 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
+    static int N;
+    static int[] nums;
+    static int[] LIS;
+    
+    public static void main(String[] args) throws IOException{
         // 코드를 작성해주세요
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int nums[] = new int[n];
-        int dp[] = new int[n];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        for(int i=0; i<n; i++){
-            nums[i] = sc.nextInt();
+        N = Integer.valueOf(br.readLine());
+        nums = new int[N];
+        LIS = new int[N];
+        
+        String[] input = br.readLine().split(" ");
+        for(int i=0; i<N; i++){
+            nums[i] = Integer.valueOf(input[i]);
         }
         
-        int max = 1;
-        for(int i=0; i<n; i++){
-            dp[i] = 1;
-            for(int j=0; j<i; j++){
-                if(nums[i] > nums[j] && dp[i] < dp[j]+1){
-                    dp[i] = dp[j] + 1;
-                }
+        int answer = 1;
+        int length = 1;
+        LIS[0] = nums[0];
+        for(int i=1; i<N; i++){
+            if(LIS[length-1] < nums[i]){
+                LIS[length++] = nums[i];
+            }else{
+                int loc = binarySearch(0, length, nums[i]);
+                // System.out.println(loc);
+                LIS[loc] = nums[i];
             }
-            max = Math.max(max, dp[i]);
+            
+            answer = Math.max(answer, length);
         }
         
-        System.out.println(max);
+        System.out.println(answer);
+        
+    }
+    
+    static int binarySearch(int st, int ed, int target){
+        int answer = ed;
+        
+        while(st <= ed){
+            int mid = (st + ed) / 2;
+            
+            if(LIS[mid] >= target){
+                ed = mid - 1;
+                answer = Math.min(answer, mid);
+            }else{
+                st = mid + 1;
+            }
+        }
+        
+        return answer;
     }
 }
