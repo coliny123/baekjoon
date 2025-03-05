2bdb3c5986a74782b0ba47613044d5cdab971237
김도환 <87745117+coliny123@users.noreply.github.com>
2025-03-06 01:09:33 +0900

    [Gold IV] Title: 알파벳, Time: 1064 ms, Memory: 16372 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -3,8 +3,9 @@ import java.io.*;
 
 public class Main {
     static int R, C;
-    static char[][] grid;
+    static int[][] grid;
     static boolean[][] visited;
+    static boolean[] alpha = new boolean[26];
     static int[] dx = {0, 0, -1, 1};
     static int[] dy = {1, -1, 0, 0};
     static int answer = 0;
@@ -17,40 +18,33 @@ public class Main {
         C = Integer.valueOf(input[1]);
         
         visited = new boolean[R][C];
-        grid = new char[R][C];
+        grid = new int[R][C];
         for(int i=0; i<R; i++){
             input = br.readLine().split("");
             for(int j=0; j<C; j++){
-                grid[i][j] = input[j].charAt(0);
+                grid[i][j] = input[j].charAt(0) - 'A';
             }
         }
         
         visited[0][0] = true;
-        dfs(0, 0, String.valueOf(grid[0][0]));
+        alpha[grid[0][0]] = true;
+        dfs(0, 0, 1);
         
         System.out.println(answer);
     }
     
-    static void dfs(int x, int y, String cur){
-        answer = Math.max(answer, cur.length());
+    static void dfs(int x, int y, int len){
+        answer = Math.max(answer, len);
         
         for(int i=0; i<4; i++){
             int nx = x + dx[i];
             int ny = y + dy[i];
-            if(inRange(nx, ny) && !visited[nx][ny]){
-                boolean flag = false;
-                for(int j=0; j < cur.length(); j++){
-                    if(cur.charAt(j) == grid[nx][ny]){
-                        flag = true;
-                        break;
-                    }
-                }
-                
-                if(!flag){
-                    visited[nx][ny] = true;
-                    dfs(nx, ny, cur+grid[nx][ny]);
-                    visited[nx][ny] = false;
-                }
+            if(inRange(nx, ny) && !visited[nx][ny] && !alpha[grid[nx][ny]]){
+                visited[nx][ny] = true;
+                alpha[grid[nx][ny]] = true;
+                dfs(nx, ny, len+1);
+                visited[nx][ny] = false;
+                alpha[grid[nx][ny]] = false;
             }
         }
     }
