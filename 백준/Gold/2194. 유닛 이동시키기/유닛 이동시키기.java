fc354301992be032681cca1c93a5d5b718c2a4cb
김도환 <87745117+coliny123@users.noreply.github.com>
2025-03-06 12:39:37 +0900

    [Gold IV] Title: 유닛 이동시키기, Time: 256 ms, Memory: 27020 KB -BaekjoonHub

--- 백준/Gold/2194. 유닛 이동시키기/유닛 이동시키기.java
+++ 백준/Gold/2194. 유닛 이동시키기/유닛 이동시키기.java
@@ -2,14 +2,9 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static int N, M, A, B, K, sX, sY, eX, eY;
-    public static int[][] grid;
-    public static int[][] step;
-    public static boolean[][] visited;
-    public static int[] dx = {0,0,-1,1};
-    public static int[] dy = {-1,1,0,0};
-    public static Queue<int[]> q = new LinkedList<>();
-    
+    static int N, M, A, B, K;
+    static int[][] grid;
+        
     public static void main(String[] args) throws IOException{
         // 코드를 작성해주세요
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,68 +16,62 @@ public class Main {
         B = Integer.valueOf(input[3]);
         K = Integer.valueOf(input[4]);
         
-        step = new int[N][M];
         grid = new int[N][M];
-        visited = new boolean[N][M];
-        
         while(K-- > 0){
             input = br.readLine().split(" ");
-            int x = Integer.valueOf(input[0])-1;
-            int y = Integer.valueOf(input[1])-1;
-            
+            int x = Integer.valueOf(input[0]) - 1;
+            int y = Integer.valueOf(input[1]) - 1;
             grid[x][y] = 1;
         }
         
         input = br.readLine().split(" ");
-        sX = Integer.valueOf(input[0])-1;
-        sY = Integer.valueOf(input[1])-1;
-        
+        int s_x = Integer.valueOf(input[0]) - 1;
+        int s_y = Integer.valueOf(input[1]) - 1;
         input = br.readLine().split(" ");
-        eX = Integer.valueOf(input[0])-1;
-        eY = Integer.valueOf(input[1])-1;
+        int e_x = Integer.valueOf(input[0]) - 1;
+        int e_y = Integer.valueOf(input[1]) - 1;
         
-        visited[sX][sY] = true;
-        q.add(new int[]{sX, sY});
-        BFS();
-        
-        
-        // for(int i=0; i<N; i++){
-            // for(int j=0; j<M; j++){
-                // System.out.print(step[i][j] + " ");
-            // }
-            // System.out.println();
-        // }
-        
-        if(step[eX][eY] == 0){
-            System.out.println(-1);
-        }else{
-            System.out.println(step[eX][eY]);
-        }
-        
+        System.out.println(bfs(s_x, s_y, e_x, e_y));
     }
     
-    
-    public static void BFS(){
+    static int bfs(int s_x, int s_y, int e_x, int e_y){
+        int[] dx = {0,0,-1,1};
+        int[] dy = {-1,1,0,0};
+        Queue<int[]> q = new LinkedList<>();
+        boolean[][] visited = new boolean[N][M];
+        
+        if(canGo(s_x, s_y, visited)){
+            q.add(new int[]{s_x, s_y, 0});
+            visited[s_x][s_y] = true;
+        }
+        
+        int answer = -1;
         while(!q.isEmpty()){
             int[] cur = q.poll();
+            
+            if(cur[0] == e_x && cur[1] == e_y){
+                answer = cur[2];
+                break;
+            }
             
             for(int i=0; i<4; i++){
                 int nx = cur[0] + dx[i];
                 int ny = cur[1] + dy[i];
-                if(canGo(nx, ny)){
-                    q.add(new int[]{nx, ny});
-                    step[nx][ny] = step[cur[0]][cur[1]]+1;
+                if(canGo(nx, ny, visited)){
+                    q.add(new int[]{nx, ny, cur[2]+1});
                     visited[nx][ny] = true;
-                }
+                }                
             }
         }
+        
+        return answer;
     }
     
-    public static boolean inRange(int x, int y){
-        return (0<=x && x+A-1<N && 0<=y && y+B-1<M);
+    static boolean inRange(int x, int y){
+        return (0 <= x && x < N-A+1 && 0 <= y && y < M-B+1);
     }
     
-    public static boolean canGo(int x, int y){
+   static boolean canGo(int x, int y, boolean[][] visited){
         if(!inRange(x, y)) return false;
         if(visited[x][y]) return false;
         for(int i=x; i<=x+A-1; i++){
