3e5e7fb72c115fdb554f686d1b38ef0ea79fc8f6
김도환 <87745117+coliny123@users.noreply.github.com>
2025-03-09 21:55:32 +0900

    [Gold III] Title: 파티, Time: 612 ms, Memory: 55848 KB -BaekjoonHub

---
+++ 백준/Gold/1238. 파티/파티.java
@@ -0,0 +1,86 @@
+import java.util.*;
+import java.io.*;
+
+class Node implements Comparable<Node>{
+    int idx, weight;
+    
+    public Node(int idx, int weight){
+        this.idx=idx;
+        this.weight=weight;
+    }
+    
+    @Override
+    public int compareTo(Node o){
+        return weight - o.weight;
+    }
+}
+
+public class Main {
+    static int N, M, X;
+    static ArrayList<Node>[] graph;
+    static int[] dist;
+    
+    public static void main(String[] args) throws IOException{
+        // 코드를 작성해주세요
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String[] input = br.readLine().split(" ");
+        N = Integer.valueOf(input[0]);
+        M = Integer.valueOf(input[1]);
+        X = Integer.valueOf(input[2]) - 1;
+        
+        graph = new ArrayList[N];
+        for(int i=0; i<N; i++){
+            graph[i] = new ArrayList<>();
+        }
+        
+        while(M-- > 0){
+            input = br.readLine().split(" ");
+            int st = Integer.valueOf(input[0]) - 1;
+            int ed = Integer.valueOf(input[1]) - 1;
+            int w = Integer.valueOf(input[2]);
+            
+            graph[st].add(new Node(ed, w));
+        }
+        
+        int[] sum = new int[N];
+        for(int i=0; i<N; i++){
+            if(i == X) continue;
+            dist = new int[N];
+            Arrays.fill(dist, 1000000000);
+            dijkstra(i);
+            sum[i] += dist[X];
+        }
+        
+        dist = new int[N];
+        Arrays.fill(dist, 1000000000);
+        dijkstra(X);
+        
+        int max = 0;
+        for(int i=0; i<N; i++){
+            if(i == X) continue;
+            sum[i] += dist[i];
+            max = Math.max(max, sum[i]);
+        }
+        
+        System.out.println(max);
+    }
+    
+    static void dijkstra(int st){
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        dist[st] = 0;
+        pq.add(new Node(st, 0));
+        
+        while(!pq.isEmpty()){
+            Node cur = pq.poll();
+            
+            for(int i=0; i<graph[cur.idx].size(); i++){
+                Node nx = graph[cur.idx].get(i);
+                if(dist[nx.idx] > dist[cur.idx] + nx.weight){
+                    dist[nx.idx] = dist[cur.idx] + nx.weight;
+                    pq.add(new Node(nx.idx, dist[nx.idx]));
+                }
+            }
+        }
+    }
+}
