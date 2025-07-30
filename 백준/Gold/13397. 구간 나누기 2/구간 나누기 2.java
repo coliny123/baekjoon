58a0cbffda885fe96418a5ee86f2282e7a767347
김도환 <87745117+coliny123@users.noreply.github.com>
2025-07-30 22:25:10 +0900

    [Gold IV] Title: 구간 나누기 2, Time: 136 ms, Memory: 15132 KB -BaekjoonHub

--- 백준/Gold/13397. 구간 나누기 2/구간 나누기 2.java
+++ 백준/Gold/13397. 구간 나누기 2/구간 나누기 2.java
@@ -1,41 +1,50 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
     public static int N, M;
-    public static int[] nums;
+    public static int[] arr;
     
-    
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         // 코드를 작성해주세요
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
-        nums = new int[N];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        int max = 0;
+        String[] input = br.readLine().split(" ");
+        N = Integer.valueOf(input[0]);
+        M = Integer.valueOf(input[1]);
+        
+        arr = new int[N];
+        
+        input = br.readLine().split(" ");
+        
+        // int max = 0;
         for(int i=0; i<N; i++){
-            nums[i] = sc.nextInt();
-            max = Math.max(max, nums[i]);
+            arr[i] = Integer.valueOf(input[i]);
+            // max = Math.max(max, arr[i]);
         }
         
-        
-        System.out.println(parametricSearch(0, max));
+        System.out.println(parametricSearch(0, 10000));
     }
     
     
     public static int parametricSearch(int st, int ed){
         int min = ed - st;
+        
         while(st <= ed){
             int mid = (st + ed) / 2;
+            
             if(count(mid) <= M){
                 ed = mid - 1;
                 min = Math.min(min, mid);
             }else{
                 st = mid + 1;
             }
+            
         }
+        
         return min;
     }
+    
     
     public static int count(int mid){
         int cnt = 1;
@@ -43,11 +52,11 @@ public class Main {
         int max = Integer.MIN_VALUE;
         
         for(int i=0; i<N; i++){
-            min = Math.min(min, nums[i]);
-            max = Math.max(max, nums[i]);
+            min = Math.min(min, arr[i]);
+            max = Math.max(max, arr[i]);
             if(max - min > mid){
                 cnt++;
-                min = Integer.MAX_VALUE;
+                min = Integer.MAX_VALUE;;
                 max = Integer.MIN_VALUE;
                 i--;
             }
