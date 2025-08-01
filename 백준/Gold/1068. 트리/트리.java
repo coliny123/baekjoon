72ff99939115301fefbef6c93e743faf090fe862
김도환 <87745117+coliny123@users.noreply.github.com>
2025-08-01 21:43:52 +0900

    [Gold V] Title: 트리, Time: 100 ms, Memory: 14352 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.java
+++ 백준/Gold/1068. 트리/트리.java
@@ -2,64 +2,61 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static int N;
-    public static ArrayList<Integer>[] graph;
-    public static ArrayList<Integer>[] reverse;
-    public static boolean[] visited;
-    public static Queue<Integer> q = new LinkedList<>();
-     
+    static int N;
+    static List<Integer>[] graph;
+    static int root = -1;
+    static int removed = -1;
+    static int answer = 0;
+    
     public static void main(String[] args) throws IOException{
-        // 코드를 작성해주세요
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         N = Integer.valueOf(br.readLine());
         
-        visited = new boolean[N];
         graph = new ArrayList[N];
-        reverse = new ArrayList[N];
-        for(int i=0; i<N; i++){
+        
+        for(int i = 0; i < N; i++){
             graph[i] = new ArrayList<>();
-            reverse[i] = new ArrayList<>();
         }
         
-        int root = 0;
         String[] input = br.readLine().split(" ");
-        for(int i=0; i<input.length; i++){
-            int parents = Integer.valueOf(input[i]);
-            if(parents == -1){
+        
+        for(int i = 0; i < N; i++){
+            int parent = Integer.valueOf(input[i]);
+            if(parent == -1) {
                 root = i;
-            }else{
-                graph[parents].add(i);
-                reverse[i].add(parents);
-            }
-        }
-        int remove = Integer.valueOf(br.readLine());
-        graph[remove] = new ArrayList<>();
-        for(int i=0; i<reverse[remove].size(); i++){
-            int parent = reverse[remove].get(i);
-            graph[parent].remove(Integer.valueOf(remove));
-        }
-        
-        
-        visited[root] = true;
-        q.add(root);
-        
-        int leefCnt = 0;
-        while(!q.isEmpty()){
-            int cur = q.poll();
-            if(graph[cur].size() == 0 && cur != remove) leefCnt++;
-            // System.out.println("cur = " + cur + " / size = " + graph[cur].size() + " / root = " + root + " / leefCnt = " + leefCnt);
-            
-            for(int i=0; i<graph[cur].size(); i++){
-                int nx = graph[cur].get(i);
-                if(!visited[nx]){
-                    visited[nx] = true;
-                    q.add(nx);
-                }
+            } else {
+                graph[parent].add(i);
             }
         }
         
-        System.out.println(leefCnt);
+        removed = Integer.valueOf(br.readLine());
         
+        if(removed == root) {
+            System.out.println(0);
+            return;
+        }
+        
+        dfs(root);
+        
+        System.out.println(answer);
+    }
+    
+    public static void dfs(int cur){
+        if(cur == removed) return;
+        
+        int childCount = 0;
+        
+        for(int child : graph[cur]) {
+            if(child != removed) {
+                childCount++;
+                dfs(child);
+            }
+        }
+        
+        // 자식이 없으면 리프 노드
+        if(childCount == 0) {
+            answer++;
+        }
     }
 }
