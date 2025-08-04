3664ddf37326333d18872d266b860c862396b53d
김도환 <87745117+coliny123@users.noreply.github.com>
2025-08-04 23:13:52 +0900

    [Gold V] Title: 최소비용 구하기, Time: 588 ms, Memory: 65596 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,79 +1,76 @@
 import java.util.*;
+import java.io.*;
 
-class Node {
-    int idx, value;
+
+class Node{
+    int idx, w;
     
-    public Node(int idx, int value){
+    public Node(int idx, int w){
         this.idx=idx;
-        this.value=value;
+        this.w=w;
     }
 }
 
 public class Main {
-    public static int N;
-    public static int M;
-    public static int dp[];
-    public static ArrayList<Node> graph[];
-    public static PriorityQueue<Node> pq = new PriorityQueue<>(new Comparator<Node>(){
-        @Override
-        public int compare(Node o1, Node o2){
-            return (o1.value - o2.value);
-        }
-    });
-     
+    public static int N, M;
+    public static List<Node>[] graph;
+    public static int[] distance;
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         // 코드를 작성해주세요
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        dp = new int[N];
-        for(int i=0; i<N; i++){
-            dp[i] = Integer.MAX_VALUE;
-        }
+        N = Integer.valueOf(br.readLine());
+        M = Integer.valueOf(br.readLine());
+        
+        distance = new int[N];
+        Arrays.fill(distance, Integer.MAX_VALUE);
         graph = new ArrayList[N];
         for(int i=0; i<N; i++){
             graph[i] = new ArrayList<>();
         }
-        sc.nextLine();
-        while(M-- > 0){
-            String input[] = sc.nextLine().split(" ");
-            int st = Integer.valueOf(input[0])-1; 
-            int ed = Integer.valueOf(input[1])-1; 
-            int val = Integer.valueOf(input[2]);
-            graph[st].add(new Node(ed, val));
+        
+        for(int i=0; i<M; i++){
+            String[] input = br.readLine().split(" ");
+            int st = Integer.valueOf(input[0]) - 1;
+            int ed = Integer.valueOf(input[1]) - 1;
+            int w = Integer.valueOf(input[2]);
+            
+            graph[st].add(new Node(ed, w));
         }
         
-        int startN = sc.nextInt() - 1;
-        int endN = sc.nextInt() - 1;
         
-        pq.add(new Node(startN, 0));
-        dp[startN] = 0;
-    
-        dijkstra();
+        String[] input = br.readLine().split(" ");
+        int st = Integer.valueOf(input[0]) - 1;
+        int ed = Integer.valueOf(input[1]) - 1;
         
-        System.out.println(dp[endN]);
+        System.out.println(Dijkstra(st, ed));
     }
     
-    public static void push(int idx){
-        for(int i=0; i<graph[idx].size(); i++){
-            Node nx = graph[idx].get(i);
-            if(dp[nx.idx] > dp[idx] + nx.value){
-                dp[nx.idx] = dp[idx] + nx.value;
-                pq.add(new Node(nx.idx, dp[idx] + nx.value));
-            }
-        }
-    }
     
-    public static void dijkstra(){
+    public static int Dijkstra(int st, int ed){
+        PriorityQueue<Node> pq = new PriorityQueue<>((a,b) -> {
+            return a.w - b.w;
+        });
+        pq.add(new Node(st, 0));
+        distance[st] = 0;
+        
         while(!pq.isEmpty()){
-            if(dp[pq.peek().idx] == pq.peek().value){
-                push(pq.peek().idx);
+            Node cur = pq.poll();
+            
+            if(distance[cur.idx] < cur.w) continue;
+            
+            // push
+            for(int i = 0; i < graph[cur.idx].size(); i++){
+                Node nx = graph[cur.idx].get(i);
+                
+                if(distance[nx.idx] > distance[cur.idx] + nx.w){
+                    distance[nx.idx] = distance[cur.idx] + nx.w;
+                    pq.add(new Node(nx.idx, distance[nx.idx]));
+                }
             }
-            pq.poll();
         }
+        
+        return distance[ed];
     }
-    
-
 }
